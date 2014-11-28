103a64c751c68a249c41820e0c5e62ca9a7c1593
Daniel Lawrence <dannyla@linux.com>
2014-11-28 18:54:49 +1100

    Added test for license year and file

---
+++ tests/test_license_year.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+import os
+import datetime
+
+
+class SimpleTestCase(TestCase):
+
+    def test_license_year(self):
+        self.assertEqual(os.path.exists('LICENSE'), True)
+        now = datetime.datetime.now()
+        current_year = datetime.datetime.strftime(now, '%Y')
+        license_text = open('LICENSE').read()
+        expected_text = 'Copyright (c) %s Daniel Lawrence' % current_year
+        self.assertIn(expected_text, license_text)
