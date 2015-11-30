9374c916c4672487473d7b97baee2a79978be16a
Daniel Lawrence <dannyla@linux.com>
2015-11-29 23:25:30 -0800

    Add test to make sure that the current year is used in the template

---
+++ tests/test_template_copy_year.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+import os
+import datetime
+
+
+class SimpleTestCase(TestCase):
+
+    def test_license_year(self):
+        BASE_TEMPLATE = "./vutman/templates/base.html"
+        self.assertEqual(os.path.exists(BASE_TEMPLATE), True)
+        now = datetime.datetime.now()
+        current_year = datetime.datetime.strftime(now, '%Y')
+        license_text = open(BASE_TEMPLATE).read()
+        expected_text = 'Copyright (c) %s' % current_year
+        self.assertIn(expected_text, license_text)
